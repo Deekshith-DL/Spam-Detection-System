d46fb9de5e44f296a7380b770b11bc92dc35083e
Deekshith-DL <124920286+Deekshith-DL@users.noreply.github.com>
2023-12-11 22:56:11 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/SMS-Spam-Detection/master/spam.csv", encoding= 'latin-1')
+data = pd.read_csv("spam.csv", encoding= 'latin-1')
 data = data[["class", "message"]]
 x = np.array(data["message"])
 y = np.array(data["class"])
